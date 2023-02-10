388b0ec1bb0e9c5dafd496236d5ca6d2a0aed7eb
ChefJoJo <94336009+chef-jojo@users.noreply.github.com>
2023-02-10 17:51:11 +0800

    fix

--- packages/smart-router/evm/getStableSwapPairs.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest'
-import { stableSwapPairsByChainId } from './getStableSwapPairs'
-
-describe('#getStableSwapPairs', () => {
-  it('stableSwapPairs', () => {
-    expect(stableSwapPairsByChainId).toMatchInlineSnapshot()
-  })
-})
